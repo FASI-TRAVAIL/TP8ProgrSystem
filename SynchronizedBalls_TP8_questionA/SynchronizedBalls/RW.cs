98de6afa205feba3a242c240c9b7bbbb61b6035a
FASI-TRAVAIL <84735457+FASI-TRAVAIL@users.noreply.github.com>
2021-06-05 22:26:45 +0100

    Add files via upload

---
+++ SynchronizedBalls_TP8_questionA/SynchronizedBalls/RW.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SynchronizedBalls
+{
+    public class RW
+    {
+        private int ReadersInCS;
+        private int WritersInCS;
+        private readonly object RWLock = new object();
+
+        public RW()
+        {
+            
+        }
+
+        public void EnterReader()
+        {
+
+            lock (RWLock)
+            {
+
+                while (WritersInCS == 1)
+                {
+                    Monitor.Wait(RWLock);
+                }
+                    
+                ReadersInCS++;
+
+            }
+
+        }
+
+        public void ExitReader()
+        {
+
+            lock (RWLock)
+            {
+
+                ReadersInCS--;
+
+                if (ReadersInCS == 0)
+
+                    Monitor.Pulse(RWLock);
+
+            }
+
+        }
+
+        public void EnterWriter()
+        {
+            lock (RWLock)
+            {
+
+                while (WritersInCS == 1  || ReadersInCS > 0)
+                {
+                    Monitor.Wait(RWLock);
+                }
+
+                
+                WritersInCS++;
+   
+
+            }
+
+        }
+
+        public void ExitWriter()
+        {
+
+            lock (RWLock)
+            {
+
+                WritersInCS--;
+
+                Monitor.PulseAll(RWLock);
+
+            }
+ 
+        }
+
+
+
+    }
+
+
+
+
+
+}
